6c604d1bdd6289389cbd79430f0d779d3fc183c7
bbids <82536500+bbids@users.noreply.github.com>
2024-04-02 08:31:40 +0200

    add spying on response to remove flaky tests

--- e2e/tests/game_UI.spec.js
+++ e2e/tests/game_UI.spec.js
@@ -4,24 +4,26 @@ const { test, expect, chromium } = require('@playwright/test');
   UI tests related to the game
 */
 
-test.describe('Starting a new game', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/play');
-    await page.getByRole('button', { name: 'Play' }).click();
-  });
-
-  test('Game is created', async ({ page }) => {
-    // game room loads
-    await expect(page.getByRole('heading', { name: 'Attempting' })).toBeVisible();
-  });
-
-});
-
 test.describe('Joining the game page', () => {
   let pageA;
+  let gameId;
   test.beforeEach(async () => {
     const browserA = await chromium.launch();
     pageA = await browserA.newPage();
+
+    await pageA.route('*/**/api/play/create_game', async route => {
+      const response = await route.fetch();
+
+      const body = await response.json();
+
+      gameId = body.gameId;
+      await route.fulfill({
+        status: 201,
+        contentType: 'application/json',
+        body: JSON.stringify(body)
+      });
+    });
+
     await pageA.goto('/play');
     await pageA.getByRole('button', { name: 'Play' }).click();
   });
@@ -29,7 +31,7 @@ test.describe('Joining the game page', () => {
   test('navigates both players to game', async ({ page }) => {
     await page.goto('/play');
     await page
-      .getByRole('link', { name: 'Join' })
+      .locator(`[href*="${gameId}"]`)
       .click();
 
     await expect(
--- e2e/tests/game_ws.spec.js
+++ e2e/tests/game_ws.spec.js
@@ -4,21 +4,37 @@ const { test, expect, chromium } = require('@playwright/test');
   WebSocket tests related to the game
 */
 
-test.describe.configure({ mode: 'serial' });
-
 test.describe('game websocket suite', () => {
 
   let pageA;
+  let gameId;
   test.beforeEach(async () => {
     const browserA = await chromium.launch();
     pageA = await browserA.newPage();
+
+    // spy on response to get gameId
+    await pageA.route('*/**/api/play/create_game', async route => {
+      const response = await route.fetch();
+
+      const body = await response.json();
+
+      gameId = body.gameId;
+      await route.fulfill({
+        status: 201,
+        contentType: 'application/json',
+        body: JSON.stringify(body)
+      });
+    });
+
     await pageA.goto('/play');
     await pageA.getByRole('button', { name: 'Play' }).click();
   });
 
+  // test('both clients receive GAME_READY EVENT');
+  // test('both clients reply with GAME_READY')
+
   test('joining an open game triggers GAME_START', async ({ page }) => {
     let connected = false;
-    const frames = [];
 
     page.on('websocket', ws => {
       ws.on('framereceived', data => {
@@ -26,16 +42,17 @@ test.describe('game websocket suite', () => {
         if (wsData.type === 'EVENT' && wsData.eventName === 'GAME_START') {
           connected = true;
         }
-        frames.push(1);
       });
     });
 
     await page.goto('/play');
-    await page.getByRole('link', { name: 'Join' }).click();
+    await page
+      .locator(`[href*="${gameId}"]`)
+      .click();
 
-    await expect.poll(() => frames.length).toBeGreaterThan(1);
-
-    expect(connected).toBeTruthy();
+    // think about reasonable timeout
+    // https://playwright.dev/docs/test-assertions#expectpoll
+    await expect.poll(() => connected).toBeTruthy();
   });
 
   test('leaving game page disconnects only leaver', async ({ page }) => {
@@ -52,7 +69,9 @@ test.describe('game websocket suite', () => {
       });
     })
     await page.goto('/play');
-    await page.getByRole('link', { name: 'Join' }).click();
+      await page
+      .locator(`[href*="${gameId}"]`)
+      .click();
     
     // wait for redirect to game page (sayHi)
     await page.getByText('sayHi');
