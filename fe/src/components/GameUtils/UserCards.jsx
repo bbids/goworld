0a141eb2cf26eace47d7630be14d15f815c0cc8e
bbids <82536500+bbids@users.noreply.github.com>
2024-04-18 18:14:50 +0200

    Add an appropriate background color for UserCard

--- fe/src/components/GameUtils/UserCards.jsx
+++ fe/src/components/GameUtils/UserCards.jsx
@@ -1,10 +1,27 @@
+import { useContext, useEffect } from 'react';
 import { userCard } from './GameSideBar.module.css';
+import { UserContext } from '../../contexts/UserContext';
+
+import { black, white } from './GameSideBar.module.css';
 
 const UserCards = () => {
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
+  const userClass = user.color === 'black' ? black : white;
+  const opponentClass = user.color === 'black' ? white : black;
+
   return (
     <div id='userCards'>
-      <div className={userCard}>You</div>
-      <div className={userCard}>B</div>
+      <div
+        className={`${userCard} ${userClass}`}
+      >You</div>
+      <div
+        className={`${userCard} ${opponentClass}`}
+      >Opponent</div>
     </div>
   );
 };
